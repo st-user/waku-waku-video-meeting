91167519a66a2971d687778208b805a0a19f83ea
Tomoki Sato <nck.cd.339@gmail.com>
2022-05-04 13:34:03 +0900

    Add a docker-compose file for a simple production env

--- make-package.sh
+++ make-package.sh
@@ -2,6 +2,7 @@
 
 MY_ACCOUNT_ID=$1;
 MY_REGION=$2;
+COMPOSE_FILE=$3;
 
 if [ "${MY_ACCOUNT_ID}" = "" ];
 then
@@ -15,13 +16,18 @@ then
 	exit 1
 fi
 
+if [ "${COMPOSE_FILE}" = "" ];
+then
+	COMPOSE_FILE=docker-compose-aws.yml
+fi
+
 rm -rf waku-waku-video-meeting
 rm -rf waku-waku-video-meeting.tar.gz
 mkdir waku-waku-video-meeting
 mkdir waku-waku-video-meeting/auth
 mkdir waku-waku-video-meeting/sfu
 
-cp docker-compose-aws.yml waku-waku-video-meeting/docker-compose.yml
+cp ${COMPOSE_FILE} waku-waku-video-meeting/docker-compose.yml
 REPEL_EXP_1=s/MY_ACCOUNT_ID/${MY_ACCOUNT_ID}/g
 REPEL_EXP_2=s/MY_REGION/${MY_REGION}/g
 gsed -i ${REPEL_EXP_1} waku-waku-video-meeting/docker-compose.yml
